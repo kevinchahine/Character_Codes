373dadb270fe5d17eb3815cbc0c481cad42d6c3f
Kevin Chahine <kchahine17@gmail.com>
2019-08-18 09:23:23 -0400

    Initial Commit

---
+++ Header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace ASCII
+{
+	//extern const size_t LINES_SIZE;
+	//extern char lines[];
+
+	char lines[] = {
+		'a'
+	};
+
+	const size_t LINES_SIZE = sizeof(lines) / sizeof(char);
+}
---
+++ Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "ASCII_Lines.h"
+
+using namespace std;
+using namespace ASCII;
+
+int main()
+{
+	/*for (size_t i = 0; i < 256; i++) {
+		cout << setw(4) << i
+			<< (i % 2 == 0 ? ' ' : '\t')
+			<< static_cast<char>(i) << '\n';
+	}*/
+
+	unsigned char lines[] = {
+		LINES[L | R],
+		LINES[D | R],
+		LINES[D | L],
+		LINES[D | L | R],
+		LINES[U | R],
+		LINES[U | L],
+		LINES[U | L | R],
+		LINES[U | D],
+		LINES[U | D | R],
+		LINES[U | D | L],
+		LINES[U | D | L | R],
+		LINES[DR | D | R],
+		/*LINES[DR | ],
+		LINES[DR | ],
+		LINES[DL | ],
+		LINES[DL | ],
+		LINES[DL | ],
+		LINES[DL | DR | ],
+		LINES[DL | DR | ],
+		LINES[DL | DR | ],
+		LINES[DL | DR | ],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],
+		LINES[],*/
+	};
+
+	cout << setfill(' ');
+	for (size_t i = 0; i < sizeof(lines); i++) {
+		cout << setw(4) << static_cast<unsigned int>(lines[i]) << ' ' << lines[i] << '\n';
+	}
+
+	system("pause");
+	return 0;
+}
